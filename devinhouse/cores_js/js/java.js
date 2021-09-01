3e91780fa8248f343938482050812ffa2249c782
almirdapper <almirdapper@hotmail.com>
2021-09-01 17:33:07 -0300

    coresCom JS

---
+++ devinhouse/cores_js/js/java.js
@@ -0,0 +1,9 @@
+function trocarFundoPreto(){
+	let x = document.getElementsByClassName('corFundo')[0].value
+	document.getElementById('divPreta').style.backgroundColor =x
+}
+
+function trocarFundoBranco(){
+	let x = document.getElementsByClassName('corFundo')[0].value
+	document.getElementById('divBranca').style.backgroundColor =x
+}
